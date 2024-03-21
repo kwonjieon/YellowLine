0c3f5b38565498a25d68cfbc330d529dc6c02e46
BellTigerLee <svenginedevnov11@naver.com>
2024-03-21 17:39:03 +0900

    feat: 서버 사진요청 받고 저장완료.
    DB에 이미지 저장용 Table생성됨
    추후 코드 수정 및 Table제거 예정
    yl/img로 파일넣고 전송하면 원본이미지 media폴더에다운
    이미지 스케일은 나중에 줄이는 방안을 마련하겠습니다.
    근데 어차피 이미지 저장은 안할거라 생각해서 지금고려안했습니다.

--- BACKEND/YellowLineProject/YellowLine/models.py
+++ BACKEND/YellowLineProject/YellowLine/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Post(models.Model):
+    # title = models.CharField(max_length=200)
+    # modify_date = models.DateTimeField(null=True, blank=True)
+    image = models.ImageField(default='media/default_img.jpg')
