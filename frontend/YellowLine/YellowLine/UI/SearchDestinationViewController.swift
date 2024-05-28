b56f5db2b440208eee8a1de4d5679daf71c5e37c
SeongHuiJung <tjdgml150237@naver.com>
2024-05-29 00:42:59 +0900

    feat : ui 디자인 수정중

--- frontend/YellowLine/YellowLine/UI/SearchDestinationViewController.swift
+++ frontend/YellowLine/YellowLine/UI/SearchDestinationViewController.swift
@@ -59,67 +59,43 @@ class SearchDestinationViewController: UIViewController, TMapViewDelegate {
         self.mapView?.delegate = self
         self.mapView?.setApiKey(apiKey)
         
-        listTableView.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.11, alpha: 1.00)
-        //listTableView.backgroundColor = .none
+        listTableView.backgroundColor = .white
         
         listTableView.delegate = self
         listTableView.dataSource = self
         
-        self.view.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.11, alpha: 1.00)
+        self.view.backgroundColor = .white
         //setNaviBar()
         setSearchBar()
         setNavivgationBar()
     }
     
     func setNavivgationBar() {
-        navigationBar.frame = CGRect(x: 0, y: 0, width: 393, height: 128)
-        navigationBar.layer.cornerRadius = 10
-        var shadows = UIView()
-        shadows.frame = navigationBar.frame
-        shadows.clipsToBounds = false
-        navigationBar.addSubview(shadows)
-        
-        let shadowPath0 = UIBezierPath(roundedRect: shadows.bounds, cornerRadius: 20)
-        let layer0 = CALayer()
-        layer0.shadowPath = shadowPath0.cgPath
-        layer0.shadowColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.25).cgColor
-        layer0.shadowOpacity = 1
-        layer0.shadowRadius = 25
-        layer0.shadowOffset = CGSize(width: 0, height: 0)
-        layer0.bounds = shadows.bounds
-        layer0.position = shadows.center
-        shadows.layer.addSublayer(layer0)
-        
-        var shapes = UIView()
-        shapes.frame = navigationBar.frame
-        shapes.clipsToBounds = true
-        navigationBar.addSubview(shapes)
-        
-        let layer1 = CALayer()
-        layer1.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-        layer1.bounds = shapes.bounds
-        layer1.position = shapes.center
-        shapes.layer.addSublayer(layer1)
-        
-        shapes.layer.cornerRadius = 20
-        
+        navigationBar.frame = CGRect(x: 0, y: 0, width: 393, height: 109)
+        navigationBar.layer.backgroundColor = UIColor(red: 1, green: 0.841, blue: 0.468, alpha: 1).cgColor
+        navigationBar.layer.cornerRadius = 20
+
         navigationBar.translatesAutoresizingMaskIntoConstraints = false
         navigationBar.widthAnchor.constraint(equalToConstant: 393).isActive = true
-        navigationBar.heightAnchor.constraint(equalToConstant: 128).isActive = true
-        
+        navigationBar.heightAnchor.constraint(equalToConstant: 109).isActive = true
+        navigationBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+        navigationBar.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
     }
     
     func setSearchBar() {
         searchBar.placeholder = "목적지를 입력해주세요"
         //searchBar.setImage(UIImage(named: "search-icon"), for: UISearchBar.Icon.search, state: .normal)
         searchBar.backgroundImage = UIImage()
+        searchBar.backgroundColor = .lightGray
         if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
             textfield.backgroundColor = UIColor.white
             textfield.textColor = UIColor.black
+            textfield.font = UIFont(name: "AppleSDGothicNeoM00-Regular", size: 16)
         }
         
         // 키보드에 return 표기
         searchBar.returnKeyType = .done
+        searchBar.layer.cornerRadius = 20
         /*
          searchBar.translatesAutoresizingMaskIntoConstraints = false
          searchBar.widthAnchor.constraint(equalToConstant: 356).isActive = true
@@ -390,68 +366,6 @@ class SearchDestinationViewController: UIViewController, TMapViewDelegate {
     func radiansToDegrees(_ radians: Double) -> Double {
         return radians * 180.0 / .pi
     }
-    
-    
-    
-    // 포인트리스트 값을 입력하면 뒷자리가 잘린(범위가 넓어진) 포인트 리스트 반환
-    // 포인트 근접인정 범위 계산 함수
-    func pointRangeChange (latitude: Double, longitude: Double) -> Coordinate {
-        let centerCoordinate = Coordinate(latitude: 37.54748588, longitude: 127.07295740) // 값 계산을 위한 임시 값
-        let radiusInMeters = 30.0 // 경로안내 범위지정, m 단위
-        
-        // 위도, 경도 범위 알아냄
-        let boundingBox = calculateBoundingBox(center: centerCoordinate, radius: radiusInMeters)
-        // 위도, 경도 범위의 반경값 = 특정 위도, 경도 값을 기준으로 몇 미터 이내까지 가도 되는가?
-        let radius = (boundingBox.maxLatitude - boundingBox.minLatitude) / 2
-        // 0.00003425
-        
-        // 반경값이 몇자리의 값인가?
-        let roundDownNumber = decimalPlaceCount(radius)
-        
-        // 원하는 소수점 자리 이후로 값 삭제
-        let resultLatitude = truncateDecimalPlaces(latitude, afterDecimalPlaces: roundDownNumber)
-        let resultLongitude = truncateDecimalPlaces(longitude, afterDecimalPlaces: roundDownNumber)
-        
-        return Coordinate(latitude: resultLatitude, longitude: resultLongitude)
-    }
-    
-    func calculateBoundingBox(center: Coordinate, radius: Double) -> (minLatitude: Double, maxLatitude: Double, minLongitude: Double, maxLongitude: Double) {
-        let latDistance = radius / 111111.0 // 1도의 위도 차이는 약 111,111 미터
-        let lonDistance = radius / (111111.0 * cos(degreesToRadians(center.latitude))) // 경도는 위도에 따라 변할 수 있음
-        
-        let minLatitude = center.latitude - latDistance
-        let maxLatitude = center.latitude + latDistance
-        let minLongitude = center.longitude - lonDistance
-        let maxLongitude = center.longitude + lonDistance
-        
-        return (minLatitude, maxLatitude, minLongitude, maxLongitude)
-    }
-    
-    // 소수점 자리수 알아내는 함수
-    func decimalPlaceCount(_ number: Double) -> Int {
-        // 절대값을 취합니다.
-        var absoluteValue = abs(number)
-        
-        // 로그를 이용해 소수 자리수를 구합니다.
-        let decimalPlace = log10(absoluteValue)
-        
-        absoluteValue = abs(decimalPlace)
-        
-        return Int(ceil(absoluteValue))
-    }
-    
-    // 특정 소수점 이후의 값을 버리는 함수
-    func truncateDecimalPlaces(_ number: Double, afterDecimalPlaces n: Int) -> Double {
-        let formatter = NumberFormatter()
-        formatter.minimumFractionDigits = 0
-        formatter.maximumFractionDigits = n
-        
-        if let truncatedString = formatter.string(from: NSNumber(value: number)) {
-            return Double(truncatedString) ?? 0.0
-        } else {
-            return 0.0
-        }
-    }
 }
 
 // 입력된 목적지 검색 시 API 요청 시도
@@ -498,7 +412,6 @@ extension SearchDestinationViewController: UITableViewDelegate, UITableViewDataS
             return destinationList.count
         }
         else {
-            print ("개수 ok")
             return searchHistoryList.count
         }
         
@@ -509,9 +422,9 @@ extension SearchDestinationViewController: UITableViewDelegate, UITableViewDataS
             let cell = tableView.dequeueReusableCell(withIdentifier: "DestinationCell", for: indexPath)as! DestinationCell
             cell.locationLabel.text = destinationList[indexPath.row]
             cell.locationLabel.textColor = .black
-            cell.locationLabel.font = UIFont(name: "AppleSDGothicNeoH", size: 5)
+            cell.locationLabel.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 18)
+            
             cell.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.11, alpha: 1.00)
-            cell.cellView.layer.cornerRadius = 10
             
             // cell 누르고 있거나 눌렀을 때 배경색 안바뀌게 유지
             let background = UIView()
@@ -524,8 +437,8 @@ extension SearchDestinationViewController: UITableViewDelegate, UITableViewDataS
             print ("데이터 불러오기 ok")
             let cell = tableView.dequeueReusableCell(withIdentifier: "DestinationCell", for: indexPath)as! DestinationCell
             cell.locationLabel.text = searchHistoryList[indexPath.row].arrival
+            cell.locationLabel.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 18)
             cell.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.11, alpha: 1.00)
-            cell.cellView.layer.cornerRadius = 10
             
             // cell 누르고 있거나 눌렀을 때 배경색 안바뀌게 유지
             let background = UIView()
