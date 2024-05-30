05be08bffa2028c41730f6ce8a29cce0061f8e34
kwonjieon <holemart@naver.com>
2024-05-30 23:01:21 +0900

    fix: 검색기록 중복해결 및 admin페이지수정

--- backend/DB/admin.py
+++ backend/DB/admin.py
@@ -8,7 +8,7 @@ from django.utils.translation import gettext_lazy as _
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('id', 'password')}),
-        (_('Personal info'), {'fields': ('name', 'phoneNum')}),
+        (_('Personal info'), {'fields': ('name', 'phoneNum', 'option', 'apns_token')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
@@ -17,7 +17,7 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('id', 'password1', 'password2', 'name', 'option', 'phoneNum'),
         }),
     )
-    list_display = ('id', 'name', 'option', 'phoneNum', 'is_staff')
+    list_display = ('id', 'name', 'option', 'phoneNum', 'is_staff', 'is_active', 'is_superuser')
     search_fields = ('id', 'name', 'phoneNum')
     ordering = ('id',)
 
